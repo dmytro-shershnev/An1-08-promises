7bbe045587536c22b1d171573ffa5ef4e8124974
Dmytro Shershnev <Dmytro_Shershnev@epam.com>
2016-10-10 22:07:47 +0300

    Lesson-08: Promises.

--- app/feature/feature.controller.js
+++ app/feature/feature.controller.js
@@ -6,9 +6,24 @@
 		.controller("Feature", Feature);
 
 	function Feature($q, tasksDataService) {
+        let $ctrl = this;
 
+        console.log('From controller');
 
-		
+        tasksDataService.getTasks()
+            .then(getTasksSuccess, getTasksError, getTasksNotification);
+
+        function getTasksSuccess(tasks) {
+            $ctrl.tasks = tasks;
+        }
+
+        function getTasksError(reason) {
+            console.log(reason);
+        }
+
+        function getTasksNotification(msg) {
+            console.log(msg);
+        }
 	}
 	
 })();
--- app/feature/feature.service.js
+++ app/feature/feature.service.js
@@ -1,14 +1,42 @@
 (function() {
-	"use strict";
+    "use strict";
 
-	angular
-		.module("feature")
-		.factory("tasksDataService", tasksDataService);
+    angular
+        .module("feature")
+        .factory("tasksDataService", tasksDataService);
 
-	function tasksDataService($q, $timeout) {
-		return {
+    function tasksDataService($q, $timeout) {
+        let tasks = [
+            {"id": "1", "action": "Estimate", "description": "Description of the task", "done": "false", "userId": "1", "userName": "Alan", "deadline": "2015-05-30", "priority": "1", "estimation": "3"}, 
+            {"id": "2", "action": "Create...", "done": "false", "userId": "2", "userName": "Bella"}, 
+            {"id": "3", "action": "Update...", "done": "false", "userId": "3", "userName": "Vardan"}, 
+            {"id": "4", "action": "Delete...", "done": "false", "userId": "4", "userName": "Galiia"}, 
+            {"id": "5", "action": "Deploy", "done": "false", "userId": "5", "userName": "Dmitriy"}
+        ];
 
-		};
-	}
-	
+        return {
+            getTasks
+        };
+
+        function getTasks() {
+            let def = $q.defer();
+
+            $timeout(() => {
+                let flag = true;
+
+                if (flag) {
+                    def.notify('Start...');
+                    def.notify('Almost finish...');
+                    def.notify('Finish');
+                    def.resolve(tasks);
+                } else {
+                    def.reject('Error tasks');
+                }
+
+            }, 200);
+
+            return def.promise;
+        }
+    }
+
 })();
